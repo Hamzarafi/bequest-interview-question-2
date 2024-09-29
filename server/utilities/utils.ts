a706391b620708706228a9bd07322d9971567f5c
hamzarafi <hamzarafi62@gmail.com>
2024-09-29 15:17:39 -0400

    feat: add hashing function

--- server/utilities/utils.ts
+++ server/utilities/utils.ts
@@ -1,5 +1,6 @@
+import crypto from "crypto";
 
 // Utility function to generate a SHA-256 hash for a given string
 export const generateHash = (data: string) => {
-  return ""; // #TODO: create hash function
+  return crypto.createHash("sha256").update(data).digest("hex");
 };
