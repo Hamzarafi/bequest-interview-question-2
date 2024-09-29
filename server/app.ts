38f017c6a2f2bfe0e4c8c8ae8fa81b20b1b3a0d8
hamzarafi <hamzarafi62@gmail.com>
2024-09-29 15:03:19 -0400

    chore : adding task todo and comments FE

--- server/app.ts
+++ server/app.ts
@@ -3,21 +3,35 @@ import cors from "cors";
 
 const PORT = 8080;
 const app = express();
+
+// Database structure to hold data, hash, and history for versioning
+// #TODO: change structure to hold hash and history
 const database = { data: "Hello World" };
 
+
+// Middleware to parse JSON and handle CORS
 app.use(cors());
 app.use(express.json());
 
+
+// #TODO: add a sanitization function for input
+
+
 // Routes
 
+// #TODO change to return current data and hash
 app.get("/", (req, res) => {
   res.json(database);
 });
 
+// #TODO update according to new data structure and validates and sanitizes input before storing
 app.post("/", (req, res) => {
   database.data = req.body.data;
   res.sendStatus(200);
 });
+
+// #TODO new route to return previous versions of the data
+
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
---
+++ server/utilities/utils.ts
@@ -0,0 +1,3 @@
+// Utility function to generate a SHA-256 hash for a given string
+// #TODO: add utility function to generate hash
+
